345ca0ac56f9ac5632d07aafeee5e98a444357e6
Sebastian Pfitzner <pfitzseb@gmail.com>
2024-04-08 14:04:46 +0200

    Update Infiltrator.jl

--- src/Infiltrator.jl
+++ src/Infiltrator.jl
@@ -10,7 +10,7 @@ using REPL.LineEdit: getproperty
 using REPL.LineEdit
 using Markdown
 
-export @infiltrate, @infiltry, @exfiltrate, @withstore, safehouse, exfiltrated, infiltrate
+export @infiltrate, @infiltry, @exfiltrate, @withstore,   safehouse, exfiltrated, infiltrate
 
 const REPL_HOOKED = Ref{Bool}(false)
 const INFILTRATION_LOCK = Ref{ReentrantLock}()
